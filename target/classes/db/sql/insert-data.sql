623b26f2a088e9840f5abd088c11d0f49b05e429
Feschenko Natalia <nata_141277@ukr.net>
2015-11-29 21:53:45 +0200

    3 version

--- target/classes/db/sql/insert-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO transactions (user, transactionDate, transactionType, sourceType, sourceSum, transactionDesc) VALUES ('Nata', '10/10/2015','Income','Salary',1000,'First part' );
-INSERT INTO transactions (user, transactionDate, transactionType, sourceType, sourceSum, transactionDesc) VALUES ('Nina', '11/11/2015','Income','Salary',1000,'First part' );
